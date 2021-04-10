7a17d784e8a473739e30af10128702ffc89a1a8c
jona <jonatanduttweiler@gmail.com>
2021-04-10 17:43:28 -0300

    Return a promise from getWeb3 making it async

--- src/lib/blockchain/getWeb3.js
+++ src/lib/blockchain/getWeb3.js
@@ -3,74 +3,54 @@ import Web3HttpProvider from 'web3-providers-http';
 import config from '../../configuration';
 
 let web3;
-const getWeb3 = () => {
-  console.log(`%c[${new Date().toISOString()}] getWeb3`,"color:red;font-weight:bold;")
-  return (
-    new Promise(async resolve => {
-      if (!web3) {
+//Devuelve una instancia de web3, inicializada con el provider correcto
+const getWeb3 = async () => {
+  console.log(`%c[${new Date().toISOString()}] getWeb3`, "color:red;font-weight:bold;")
+  if (web3) {
+    return web3;
+  }
 
-        let web3Wallet;
-        let web3Http;
-        let isThereWallet = false;
-        let walletNetworkId = null;
-        let walletIsCorrectNetwork = false;
+  let web3Wallet;
+  let web3Http;
+  let isThereWallet = false;
+  let walletNetworkId = null;
+  let walletIsCorrectNetwork = false;
 
-        if (window.ethereum) {
-          web3Wallet = new Web3(window.ethereum);
-          isThereWallet = true;
-          walletNetworkId = await web3Wallet.eth.net.getId();
-          console.log('Configuración Web3: Modern Browser Ethereum Provider.');
-        }
+  if (window.ethereum) {
+    web3Wallet = new Web3(window.ethereum);
+    isThereWallet = true;
+    walletNetworkId = await web3Wallet.eth.net.getId();
+  }
 
-        if (walletNetworkId === config.network.requiredId) {
+  if (walletNetworkId === config.network.requiredId) {
+    // La wallet está instalada y configurada con la red correcta.
+    walletIsCorrectNetwork = true;
+  } else {
+    // La wallet no está instalada o la red es incorrecta.
+    walletIsCorrectNetwork = false;
 
-          // La wallet está instalada y configurada con la red correcta.
-          walletIsCorrectNetwork = true;
+    var web3HttpProvider = new Web3HttpProvider(config.network.nodeUrl, {
+      keepAlive: true,
+      timeout: config.network.timeout,
+    });
+    web3Http = new Web3(web3HttpProvider);
+  }
 
-        } else {
+  if (web3Http) {
+    console.log('Configuración Web3: HTTP Provider.', web3HttpProvider);
+    web3 = web3Http;
+  } else {
+    console.log('Configuración Web3: Modern Browser Ethereum Provider.');
+    web3 = web3Wallet;
+  }
+  web3.isThereWallet = isThereWallet;
+  web3.walletNetworkId = walletNetworkId;
+  web3.walletIsCorrectNetwork = walletIsCorrectNetwork;
 
-          // La wallet no está instalada o la red es incorrecta.
-          walletIsCorrectNetwork = false;
+  console.log('Configuración Web3', web3);
 
-          var web3HttpProvider = new Web3HttpProvider(config.network.nodeUrl, {
-            keepAlive: true,
-            timeout: config.network.timeout,
-          });
-          web3Http = new Web3(web3HttpProvider);
-          console.log('Configuración Web3: HTTP Provider.', web3HttpProvider);
-        }
-
-        if (web3Http) {
-          web3 = web3Http;
-        } else {
-          web3 = web3Wallet;
-        }
-        web3.isThereWallet = isThereWallet;
-        web3.walletNetworkId = walletNetworkId;
-        web3.walletIsCorrectNetwork = walletIsCorrectNetwork;
-
-        console.log('Configuración Web3', web3);
-      }
-      resolve(web3);
-    })
-  )
+  return web3;
 };
 
 
 export default getWeb3;
-/*let enablePromise;
-function enable(force = false) {
-  if (!force && enablePromise) return enablePromise;
-  return new Promise((resolve, reject) =>
-    this.currentProvider.enable()
-      .then(addrs => {
-        this.isEnabled = true;
-        resolve(addrs);
-      })
-      .catch(e => {
-        enablePromise = false;
-        this.isEnabled = false;
-        reject(e);
-      }),
-  );
-}*/
