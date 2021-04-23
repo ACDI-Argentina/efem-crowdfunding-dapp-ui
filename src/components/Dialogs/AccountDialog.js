0ee509b6a44d7bd92e01faa7fa0c0daf9cd7c910
jona <jonatanduttweiler@gmail.com>
2021-04-22 21:52:05 -0300

    Improve styles of account dialog

--- src/components/Dialogs/AccountDialog.js
+++ src/components/Dialogs/AccountDialog.js
@@ -35,9 +35,49 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const LinkButton = styled.a`
-
+  color: #53a653;
+  font-weight:bold;
+  padding:10px;
+  :hover{
+    text-decoration: underline;
+    color: #53a653;
+  }
+  
+`
+const Footer = styled.div`
+  padding:10px;
 `
 
+const Address = styled.div`
+  font-family:Rubik, sans-serif;
+  font-weight:400;
+  font-size: 1.3em;
+  padding:1em;
+  color: gray;
+  word-break: break-all;
+`
+const Button = styled.button`
+  padding: .2em 1em;
+  border-radius: 1em; 
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+              0px 2px 2px 0px rgba(0, 0, 0, 0.14),
+              0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  border: 1px solid transparent;
+`
+
+const LogoutButton = styled(Button)`
+  color: white;
+  background-color: #FF5D49;
+  font-weight: bold;
+  font-size: 1.1em;
+`
+
+const CopyAddress = styled.div`
+  font-weight:bold;
+  cursor:pointer;
+  padding:10px;
+  color: #53a653;
+`
 
 
 const AccountDialog = ({ address, onClose, ...props }) => {
@@ -65,22 +105,34 @@ const AccountDialog = ({ address, onClose, ...props }) => {
 
       <div className={classes.root}>
         <Grid container spacing={3}>
-          <Grid item xs={12}>
-            {address}
+          <Grid container item justify="center" xs={12}>
+            <Address>
+              {address}
+            </Address>
           </Grid>
-          <Grid item sm={6}>
+          <Grid container justify="center" align="center" item sm={4} xs={12}>
             <LinkButton
               target="_blank"
               href={`${explorer}/address/${address}`}>
-              View on rsk explorer
+              View on RSK explorer
             </LinkButton>
+
           </Grid>
-          <Grid item sm={6}>
-            <input ref={addressRef} type="hidden" value={address}/>
-            <div>Copy address</div>
+          <Grid container justify="center" align="center" item sm={4} xs={12}>
+            <input ref={addressRef} type="hidden" value={address} />
+            <CopyAddress>Copy address <i class="far fa-copy"></i></CopyAddress>
           </Grid>
-          <Grid item xs={12}>
-            Logout button
+          <Grid
+            container
+            item
+            justify="center"
+            xs={12}
+          >
+            <Footer>
+            <LogoutButton>
+              Logout
+            </LogoutButton>
+            </Footer>
           </Grid>
 
         </Grid>
