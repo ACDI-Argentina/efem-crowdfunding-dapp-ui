788c19425af0e633ae1ef3f81e4c588f1331b035
jona <jonatanduttweiler@gmail.com>
2021-06-24 19:15:49 -0300

    Remove dead code

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styled from "styled-components";
 import { NavLink, withRouter } from 'react-router-dom';
 import { history } from '../lib/helpers';
 import { connect } from 'react-redux';
@@ -7,27 +6,11 @@ import { selectCurrentUser } from '../redux/reducers/currentUserSlice';
 import LanguageSelector from '../components/LanguageSelector'
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import Button from "components/CustomButtons/Button.js";
 import CustomDropdown from './CustomDropdown/CustomDropdown';
 import styles from "assets/jss/material-kit-react/components/headerLinksStyle.js";
 import { withStyles } from '@material-ui/core/styles';
 import { Web3AppContext } from 'lib/blockchain/Web3App';
 
-
-const signUpSwal = () => {
-  React.swal({
-    title: 'Sign Up!',
-    content: React.swal.msg(
-      <p>
-        In order to use the Dapp, you need to use a Web3 wallet.
-        <br />
-        It is recommended that you install <a href="https://metamask.io/">MetaMask</a>.
-      </p>,
-    ),
-    icon: 'info',
-    buttons: ['Ok'],
-  });
-};
 // Broken rule that can not find the correct id tag
 /* eslint jsx-a11y/aria-proptypes: 0 */
 /**
@@ -48,25 +31,15 @@ class MainMenu extends Component {
   render() {
     const { classes, currentUser } = this.props;
     const registered =  currentUser && currentUser.registered || false;
-    const { validProvider, isEnabled, failedToLoad } = this.context;
 
     return (
       <List className={classes.list}>
+
+
+
         <ListItem className={classes.listItem}>
           <LanguageSelector ></LanguageSelector>
         </ListItem>
-
-        {validProvider && !failedToLoad && !isEnabled && (
-          <ListItem className={classes.listItem}>
-            <Button
-              color="transparent"
-              className={classes.navLink}
-              onClick={signUpSwal}
-            >
-              Reg&iacute;strate!
-            </Button>
-          </ListItem>
-        )}
 
         {currentUser && (
           <ListItem className={classes.listItem}>
