978d10f3c9b045eba6fa2a2eb3a66237d0b9d470
jona <jonatanduttweiler@gmail.com>
2021-07-05 19:37:11 -0300

    Add  responsive style on dapp logo in all pages

--- src/assets/jss/material-kit-react/views/campaignPage.js
+++ src/assets/jss/material-kit-react/views/campaignPage.js
@@ -32,8 +32,14 @@ const landingPageStyle = {
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
   },
   dappLogo: {
-    maxHeight: "4em"
-  }
+    maxHeight: "4em",
+    "@media (max-width: 800px)": {
+      maxHeight: "3em"
+    },
+    "@media (max-width: 600px)": {
+      maxHeight: "2em"
+    }
+  },
 };
 
 export default landingPageStyle;
