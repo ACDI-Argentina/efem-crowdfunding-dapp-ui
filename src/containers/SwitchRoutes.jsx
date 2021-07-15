7199e32684048fadaa7a2bc1dd972d4b70801b5d
jona <jonatanduttweiler@gmail.com>
2021-07-15 19:14:59 -0300

    [Performance] Lazy load pages

--- src/containers/SwitchRoutes.jsx
+++ src/containers/SwitchRoutes.jsx
@@ -1,192 +1,146 @@
-import React from 'react'
-import { Route, Redirect, Switch } from 'react-router-dom'
-import Profile from '../components/views/Profile'
-import EditProfile from '../components/views/EditProfile'
-import ViewMilestone from '../components/views/ViewMilestone'
-import EditDAC from '../components/views/EditDAC'
-import ViewDAC from '../components/views/ViewDAC'
-import MyDACs from '../components/views/MyDACs'
-import MyCampaigns from '../components/views/MyCampaigns'
-import MyMilestones from '../components/views/MyMilestones'
-import NotFound from '../components/views/NotFound'
-import Campaigns from '../components/views/Campaigns'
-import DACs from '../components/views/DACs'
-import TermsAndConditions from '../components/views/TermsAndConditions'
-import PrivacyPolicy from '../components/views/PrivacyPolicy'
-import EditCampaign from '../components/views/EditCampaign'
-import ViewCampaign from '../components/views/ViewCampaign'
-import EditMilestone from '../components/views/EditMilestone'
-import LandingPage from "views/LandingPage/LandingPage.js"
-import LoginPage from "views/LoginPage/LoginPage.js"
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import LandingPage from 'views/LandingPage/LandingPage.js';
+import Loader from '../components/Loader';
+const Profile = React.lazy(() => import('../components/views/Profile'));
+const EditProfile = React.lazy(() => import('../components/views/EditProfile'));
+const ViewMilestone = React.lazy(() => import('../components/views/ViewMilestone'));
+const EditDAC = React.lazy(() => import('../components/views/EditDAC'));
+const ViewDAC = React.lazy(() => import('../components/views/ViewDAC'));
+const MyDACs = React.lazy(() => import('../components/views/MyDACs'));
+const MyCampaigns = React.lazy(() => import('../components/views/MyCampaigns'));
+const MyMilestones = React.lazy(() => import('../components/views/MyMilestones'));
+const NotFound = React.lazy(() => import('../components/views/NotFound'));
+const Campaigns = React.lazy(() => import('../components/views/Campaigns'));
+const DACs = React.lazy(() => import('../components/views/DACs'));
+const TermsAndConditions = React.lazy(() => import('../components/views/TermsAndConditions'));
+const PrivacyPolicy = React.lazy(() => import('../components/views/PrivacyPolicy'));
+const EditCampaign = React.lazy(() => import('../components/views/EditCampaign'));
+const ViewCampaign = React.lazy(() => import('../components/views/ViewCampaign'));
+const EditMilestone = React.lazy(() => import('../components/views/EditMilestone'));
 
-const SwitchRoutes = ({ currentUser}) => (
+const SwitchRoutes = ({ currentUser }) => (
+  <React.Suspense fallback={<Loader className="fixed"/>}>
     <Switch>
-
-        {/*NOTE order matters, wrong order breaks routes!*/}
-        <Route
-            exact
-            path="/termsandconditions"
-            render={props => <TermsAndConditions {...props} />}
-        />
-        <Route
-            exact
-            path="/privacypolicy"
-            render={props => <PrivacyPolicy {...props} />}
-        />
-        <Route
-            exact
-            path="/dacs/new"
-            render={props => (
-                <EditDAC
-                    isNew
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/dacs/:id"
-            render={props => (
-                <ViewDAC
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/dacs/:id/edit"
-            render={props => (
-                <EditDAC
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-
-        <Route
-            exact
-            path="/campaigns/new"
-            render={props => (
-                <EditCampaign
-                    isNew
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/campaigns/:id"
-            render={props => (
-                <ViewCampaign
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/campaigns/:id/edit"
-            render={props => (
-                <EditCampaign
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-
-        <Route
-            exact
-            path="/campaigns/:id/milestones/new"
-            render={props => (
-                <EditMilestone
-                    isNew
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/campaigns/:id/milestones/propose"
-            render={props => (
-                <EditMilestone
-                    isNew
-                    isProposed
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/campaigns/:id/milestones/:milestoneId"
-            render={props => (
-                <ViewMilestone
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/campaigns/:id/milestones/:milestoneId/edit/proposed"
-            render={props => (
-                <EditMilestone
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    isProposed
-                    {...props}
-                />
-            )}
-        />
-
-        <Route
-            exact
-            path="/campaigns/:id/milestones/:milestoneId/edit"
-            render={props => (
-                <EditMilestone
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/campaigns/:id/milestones"
-            render={({ match }) => (
-                <Redirect to={`/campaigns/${match.params.id}`} />
-            )}
-        />
-        <Route
-            exact
-            path="/milestones/:milestoneId/edit"
-            render={props => (
-                <EditMilestone
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/milestones/:milestoneId/edit/proposed"
-            render={props => (
-                <EditMilestone
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    isProposed
-                    {...props}
-                />
-            )}
-        />
-        {/*<Route
+      {/*NOTE order matters, wrong order breaks routes!*/}
+      <Route
+        exact
+        path="/termsandconditions"
+        render={(props) => <TermsAndConditions {...props} />}
+      />
+      <Route exact path="/privacypolicy" render={(props) => <PrivacyPolicy {...props} />} />
+      <Route exact path="/dacs/new" render={(props) => <EditDAC isNew {...props} />} />
+      <Route
+        exact
+        path="/dacs/:id"
+        render={(props) => <ViewDAC currentUser={currentUser} {...props} />}
+      />
+      <Route
+        exact
+        path="/dacs/:id/edit"
+        render={(props) => (
+          <EditDAC key={currentUser ? currentUser.id : 0} currentUser={currentUser} {...props} />
+        )}
+      />
+      <Route exact path="/campaigns/new" render={(props) => <EditCampaign isNew {...props} />} />
+      <Route
+        exact
+        path="/campaigns/:id"
+        render={(props) => <ViewCampaign currentUser={currentUser} {...props} />}
+      />
+      <Route
+        exact
+        path="/campaigns/:id/edit"
+        render={(props) => (
+          <EditCampaign
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            {...props}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/campaigns/:id/milestones/new"
+        render={(props) => (
+          <EditMilestone
+            isNew
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            {...props}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/campaigns/:id/milestones/propose"
+        render={(props) => (
+          <EditMilestone
+            isNew
+            isProposed
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            {...props}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/campaigns/:id/milestones/:milestoneId"
+        render={(props) => <ViewMilestone currentUser={currentUser} {...props} />}
+      />
+      <Route
+        exact
+        path="/campaigns/:id/milestones/:milestoneId/edit/proposed"
+        render={(props) => (
+          <EditMilestone
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            isProposed
+            {...props}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/campaigns/:id/milestones/:milestoneId/edit"
+        render={(props) => (
+          <EditMilestone
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            {...props}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/campaigns/:id/milestones"
+        render={({ match }) => <Redirect to={`/campaigns/${match.params.id}`} />}
+      />
+      <Route
+        exact
+        path="/milestones/:milestoneId/edit"
+        render={(props) => (
+          <EditMilestone
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            {...props}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/milestones/:milestoneId/edit/proposed"
+        render={(props) => (
+          <EditMilestone
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            isProposed
+            {...props}
+          />
+        )}
+      />
+      {/*<Route
             exact
             path="/delegations"
             render={props => (
@@ -197,81 +151,54 @@ const SwitchRoutes = ({ currentUser}) => (
                 />
             )}
         />*/}
-        <Route
-            exact
-            path="/my-dacs"
-            render={props => (
-                <MyDACs
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/my-campaigns"
-            render={props => (
-                <MyCampaigns
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/my-milestones"
-            render={props => (
-                <MyMilestones
-                    key={currentUser ? currentUser.id : 0}
-                    currentUser={currentUser}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/profile"
-            render={props => (
-                <EditProfile
-                    key={currentUser ? currentUser.id : 0}
-                    {...props}
-                />
-            )}
-        />
-        <Route
-            exact
-            path="/profile/:userAddress"
-            render={props => <Profile {...props} />}
-        />
-
-        <Route path="/" render={props => <LandingPage {...props} />} />
-
-        {/*<Route
+      <Route
+        exact
+        path="/my-dacs"
+        render={(props) => (
+          <MyDACs key={currentUser ? currentUser.id : 0} currentUser={currentUser} {...props} />
+        )}
+      />
+      <Route
+        exact
+        path="/my-campaigns"
+        render={(props) => (
+          <MyCampaigns
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            {...props}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/my-milestones"
+        render={(props) => (
+          <MyMilestones
+            key={currentUser ? currentUser.id : 0}
+            currentUser={currentUser}
+            {...props}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/profile"
+        render={(props) => <EditProfile key={currentUser ? currentUser.id : 0} {...props} />}
+      />
+      <Route exact path="/profile/:userAddress" render={(props) => <Profile {...props} />} />
+      <Route path="/" render={(props) => <LandingPage {...props} />} />
+      {/*<Route
             exact
             path="/"
             render={props => <Explore {...props} />}
         />*/}
-        <Route
-            exact
-            path="/campaigns"
-            render={props => <Campaigns {...props} />}
-        />
-        <Route
-            exact
-            path="/dacs"
-            render={props => <DACs {...props} />}
-        />
-
-        {/* Other material react routes. Not used*/})
-        <Route path="/landing-page" render={props => <LandingPage {...props} />} />
-        <Route path="/login-page" component={LoginPage} />
-
-
-
-        <Route component={NotFound} />
+      <Route exact path="/campaigns" render={(props) => <Campaigns {...props} />} />
+      <Route exact path="/dacs" render={(props) => <DACs {...props} />} />
+      {/* Other material react routes. Not used*/})
+      <Route path="/landing-page" render={(props) => <LandingPage {...props} />} />
+      <Route component={NotFound} />
     </Switch>
+  </React.Suspense>
 );
 
 export default SwitchRoutes;
