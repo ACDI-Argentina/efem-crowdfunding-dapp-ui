d0fce9d75b34b0d32a5f0106973548bb0e0fedee
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2021-10-16 10:57:24 -0300

    Remove unused code

--- src/components/views/LandingPage.js
+++ src/components/views/LandingPage.js
@@ -12,7 +12,6 @@ import GridItem from "components/Grid/GridItem.js";
 import Parallax from "components/Parallax/Parallax.js";
 
 // Sections for this page
-import DACs from "components/views/DACs.jsx";
 import Campaigns from "components/views/Campaigns.jsx";
 import PlatformFeatures from "components/views/PlatformFeatures.jsx";
 import BlockchainBenefits from "components/views/BlockchainBenefits.jsx";
@@ -21,13 +20,6 @@ import MainMenu from "components/MainMenu.jsx";
 
 import styles from "assets/jss/material-kit-react/views/landingPage.js";
 import { withTranslation } from 'react-i18next';
-
-import OnlyRole from 'components/OnlyRole';
-
-import { CREATE_CAMPAIGN_ROLE } from "constants/Role";
-
-import Button from "components/CustomButtons/Button.js";
-import OnlyCorrectNetwork from 'components/OnlyCorrectNetwork';
 
 const useStyles = makeStyles(styles);
 
@@ -54,18 +46,6 @@ export default withTranslation()(function LandingPage(props) {
                 <h4 className={classes.subtitle}>
                   {t('landingPagesSubtitle1')}<span>{t('landingPagesSubtitle2')}</span>{t('landingPagesSubtitle3')}
                 </h4>
-                {/*<center>
-                  <OnlyCorrectNetwork>
-                    <OnlyRole role={CREATE_CAMPAIGN_ROLE}>
-                      <Button style={{ marginRight: "2em" }} color="primary" size="m" className="btn btn-info" onClick={() => this.createCampaign()}>
-                      {t('landingPageFinanceProject')}
-                      </Button>
-                    </OnlyRole>
-                    <Button color="primary" size="m" className="btn btn-info" onClick={() => {}}>
-                      {t('landingPageMakeDonation')}
-                    </Button>
-                  </OnlyCorrectNetwork>            
-                </center>*/}
               </div>
             </GridItem>
           </GridContainer>
@@ -75,7 +55,6 @@ export default withTranslation()(function LandingPage(props) {
         <div className={classes.container}>
           <img src={require("assets/img/icons/separator.png")} alt="" className={classes.topSeparator} />
           <JoinGivethCommunity history={props.history} />
-          {/*<DACs />*/}
           <Campaigns />
           <PlatformFeatures />
           <BlockchainBenefits />
